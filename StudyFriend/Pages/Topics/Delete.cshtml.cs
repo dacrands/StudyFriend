031ad0f7012d1d00bfe774ef9ab50a9aa1cc4516
David Crandall <davecrands@gmail.com>
2019-06-22 23:44:57 -0400

    Scaffold Topic model

---
+++ StudyFriend/Pages/Topics/Delete.cshtml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using StudyFriend.Models;
+
+namespace StudyFriend.Pages.Topics
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly StudyFriend.Models.StudyFriendContext _context;
+
+        public DeleteModel(StudyFriend.Models.StudyFriendContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Topic Topic { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Topic = await _context.Topic.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (Topic == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Topic = await _context.Topic.FindAsync(id);
+
+            if (Topic != null)
+            {
+                _context.Topic.Remove(Topic);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ StudyFriend/Pages/Topics/Details.cshtml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using StudyFriend.Models;
+
+namespace StudyFriend.Pages.Topics
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly StudyFriend.Models.StudyFriendContext _context;
+
+        public DetailsModel(StudyFriend.Models.StudyFriendContext context)
+        {
+            _context = context;
+        }
+
+        public Topic Topic { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Topic = await _context.Topic.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (Topic == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
