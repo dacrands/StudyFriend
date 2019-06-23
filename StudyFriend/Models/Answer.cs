8e49e6795792b4446fbc14dd88516ad318b8c236
David Crandall <davecrands@gmail.com>
2019-06-22 21:56:16 -0400

    Add Answer model

---
+++ StudyFriend/Models/Answer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudyFriend.Models
+{
+    public class Answer
+    {
+        public int AnswerID { get; set; }
+        public int QuestionID { get; set; }
+        public string Body { get; set; }
+        public Question Question { get; set; }
+    }
+}
