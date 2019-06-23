b339e3617b0bac7d18cca7050b95ff13769fb67b
David Crandall <davecrands@gmail.com>
2019-06-22 21:57:03 -0400

    Add Topic model

---
+++ StudyFriend/Models/Topic.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudyFriend.Models
+{
+    public class Topic
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public ICollection<Question> Questions { get; set; }
+    }
+}
