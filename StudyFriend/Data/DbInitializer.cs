55d7a86b7a31d17fbd317a34ba4db8eff674108f
David Crandall <davecrands@gmail.com>
2019-06-22 21:55:53 -0400

    Add DbInitializer

---
+++ StudyFriend/Data/DbInitializer.cs
@@ -0,0 +1,54 @@
+﻿using StudyFriend.Models;
+using System;
+using System.Linq;
+
+namespace StudyFriend.Models
+{
+    public static class DbInitializer
+    {
+        public static void Initialize(StudyFriendContext context)
+        {
+            context.Database.EnsureCreated();
+            // If any Questions exist, don't reseed the DB
+
+            if (context.Question.Any())
+            {
+                return;
+            }
+
+
+            
+
+            var topics = new Topic[]
+            {
+                new Topic { Name = "Life"}
+            };
+            foreach (Topic t in topics)
+            {
+                context.Topic.Add(t);
+            }
+            context.SaveChanges();
+
+            
+            var questions = new Question[]
+            {
+                new Question{ Body="Will this even work?", TopicID=1 }
+            };
+            foreach (Question q in questions)
+            {
+                context.Question.Add(q);
+            }
+            context.SaveChanges();
+
+            var answers = new Answer[]
+            {
+                new Answer { QuestionID = 1, Body ="Probably not" }
+            };
+            foreach (Answer a in answers)
+            {
+                context.Answer.Add(a);
+            }
+            context.SaveChanges();
+        }
+    }
+}
