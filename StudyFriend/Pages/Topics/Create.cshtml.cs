62a36b3502653237b8df68f1c3299066cd74dfda
da crands <davecrands@gmail.com>
2019-10-05 12:44:54 -0400

    Add userId FK to Topic on create Topic

--- StudyFriend/Pages/Topics/Create.cshtml.cs
+++ StudyFriend/Pages/Topics/Create.cshtml.cs
@@ -2,16 +2,20 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using StudyFriend.Models;
+using Microsoft.AspNetCore.Identity;
 
 namespace StudyFriend.Pages.Topics
 {
     public class CreateModel : PageModel
     {
         private readonly StudyFriendContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public CreateModel(StudyFriendContext context)
+        public CreateModel(StudyFriendContext context,
+            UserManager<ApplicationUser> userManager)
         {
             _context = context;
+            _userManager = userManager;
         }
 
         public IActionResult OnGet()
@@ -28,6 +32,10 @@ namespace StudyFriend.Pages.Topics
             {
                 return Page();
             }
+
+            var currUser = await _userManager.GetUserAsync(User);
+
+            Topic.UserId = currUser.Id;
 
             _context.Topic.Add(Topic);
             await _context.SaveChangesAsync();
