71e437f5272270cfd1293c318fdda3df1b9aa5a0
da crands <davecrands@gmail.com>
2019-10-05 12:47:06 -0400

    Update query to only show a user's Topics

--- StudyFriend/Pages/Topics/Index.cshtml.cs
+++ StudyFriend/Pages/Topics/Index.cshtml.cs
@@ -1,25 +1,35 @@
 ﻿using System.Collections.Generic;
 using System.Threading.Tasks;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 using StudyFriend.Models;
+
 
 namespace StudyFriend.Pages.Topics
 {
     public class IndexModel : PageModel
     {
         private readonly StudyFriendContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public IndexModel(StudyFriendContext context)
+        public IndexModel(StudyFriendContext context,
+            UserManager<ApplicationUser> userManager)
         {
             _context = context;
+            _userManager = userManager;
         }
-
-        public IList<Topic> Topic { get;set; }
-
+        
+        public IList<Topic> Topic { get; set; }
+        
         public async Task OnGetAsync()
         {
-            Topic = await _context.Topic.ToListAsync();
+            var currUser = await _userManager.GetUserAsync(User);
+            var topics = from t in _context.Topic
+                         select t;
+            topics = topics.Where(t => t.UserId == currUser.Id);
+            Topic = await topics.ToListAsync();
         }
     }
 }
