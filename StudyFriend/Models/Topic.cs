155e48ffbae7e66bb376daee8b8df2c65637b883
da crands <davecrands@gmail.com>
2019-10-05 12:36:52 -0400

    Add ApplicationUser, FK to Topic model

--- StudyFriend/Models/Topic.cs
+++ StudyFriend/Models/Topic.cs
@@ -4,8 +4,10 @@ namespace StudyFriend.Models
 {
     public class Topic
     {
-        public int ID { get; set; }
+        public int TopicID { get; set; }
+        public string UserId { get; set; }
         public string Name { get; set; }
+        public ApplicationUser ApplicationUser { get; set; }
         public ICollection<Question> Questions { get; set; }
     }
 }
