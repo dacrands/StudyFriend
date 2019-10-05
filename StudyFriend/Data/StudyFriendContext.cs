d7ffac616c73b220b6f11b81bd86d1867877b8b5
da crands <davecrands@gmail.com>
2019-10-05 12:32:07 -0400

    Create many-to-one rel in OnModelCreating

--- StudyFriend/Data/StudyFriendContext.cs
+++ StudyFriend/Data/StudyFriendContext.cs
@@ -14,5 +14,31 @@ namespace StudyFriend.Models
         public DbSet<Question> Question { get; set; }
         public DbSet<Answer> Answer { get; set; }
         public DbSet<Topic> Topic { get; set; }
+
+
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<ApplicationUser>(
+                typeBuilder =>
+                {
+                    typeBuilder.HasMany(user => user.Topics)
+                        .WithOne(topic => topic.ApplicationUser)
+                        .HasForeignKey(topic => topic.UserId)
+                        .IsRequired();
+                });
+
+            builder.Entity<Topic>(
+                typeBuilder =>
+                {
+                    typeBuilder.HasOne(topic => topic.ApplicationUser)
+                        .WithMany(user => user.Topics)
+                        .HasForeignKey(topic => topic.UserId)
+                        .IsRequired();
+                });
+        }
+
     }
 }
