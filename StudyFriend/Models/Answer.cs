c27a0e7bc74ddfc5c4e02a61cd29464497464b87
da crands <davecrands@gmail.com>
2019-11-09 21:01:55 -0500

    Add Answer model data validation

--- StudyFriend/Models/Answer.cs
+++ StudyFriend/Models/Answer.cs
@@ -1,10 +1,17 @@
-﻿namespace StudyFriend.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudyFriend.Models
 {
     public class Answer
     {
         public int AnswerID { get; set; }
+
         public int QuestionID { get; set; }
+
+        [StringLength(320, ErrorMessage = "{0} length must be between {2} and {1}.", MinimumLength = 1)]
+        [Required(ErrorMessage = "Please provide an answer.")]
         public string Body { get; set; }
+
         public Question Question { get; set; }
     }
 }
