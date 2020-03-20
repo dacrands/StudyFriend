5421a7a3d2787b2b7f2918da17081cc6919d306c
da crands <davecrands@gmail.com>
2020-03-20 18:35:26 -0400

    Add ApplicationUser to seed data

--- StudyFriend/Data/DbInitializer.cs
+++ StudyFriend/Data/DbInitializer.cs
@@ -1,25 +1,34 @@
-﻿using StudyFriend.Models;
+﻿using Microsoft.AspNetCore.Identity;
+using StudyFriend.Models;
+using System.Linq;
+using System.Security.Policy;
 
 namespace StudyFriend.Data
 {
     public static class DbInitializer
-    {
-        public static void Initialize(StudyFriendContext context)
+    
+    {        
+        public static async System.Threading.Tasks.Task InitializeAsync(
+            StudyFriendContext context, 
+            UserManager<ApplicationUser> userManager
+            )
         {
-            context.Database.EnsureCreated();
-            // If any Questions exist, don't reseed the DB
 
-            if (true)
+            context.Database.EnsureCreated();
+
+            // Don't seed db if test ApplicationUser exists
+            if (context.ApplicationUser.Any())
             {
                 return;
             }
 
-
-            
+            var user = new ApplicationUser { UserName = "test@test.com", Email = "test@test.com" };
+            _ = await userManager.CreateAsync(user, "P@ssw0rd1!");
+            context.SaveChanges();
 
             var topics = new Topic[]
             {
-                new Topic { Name = "Life"}
+                new Topic { Name = "JavaScript", UserId = user.Id}
             };
             foreach (Topic t in topics)
             {
@@ -30,7 +39,7 @@ namespace StudyFriend.Data
             
             var questions = new Question[]
             {
-                new Question{ Body="Will this even work?", TopicID=1 }
+                new Question{ Body="How do you stop event bubbling?", TopicID=topics[0].TopicID }
             };
             foreach (Question q in questions)
             {
@@ -40,7 +49,7 @@ namespace StudyFriend.Data
 
             var answers = new Answer[]
             {
-                new Answer { QuestionID = 1, Body ="Probably not" }
+                new Answer { Body="event.stopPropagation()", QuestionID = questions[0].QuestionID }
             };
             foreach (Answer a in answers)
             {
