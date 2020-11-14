752bf600d37e789bca519e97408e4f5600c437c6
David Crandall <davecrands@gmail.com>
2020-11-14 11:04:26 -0600

    Update Auth page tests to ensure no redirect

--- StudyFriendTests/AuthTests.cs
+++ StudyFriendTests/AuthTests.cs
@@ -1,5 +1,13 @@
-﻿using Microsoft.AspNetCore.Mvc.Testing;
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using System.Net;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using System.Text.Encodings.Web;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -35,6 +43,60 @@ namespace StudyFriendTests
             Assert.Equal(HttpStatusCode.Redirect, response.StatusCode);
             Assert.Equal("/Identity/Account/Login",
                 response.Headers.Location.LocalPath);
+        }
+
+        [Theory]
+        [InlineData("/Topics")]
+        [InlineData("/Questions")]
+        [InlineData("/Answers")]
+        public async Task Get_SecurePageIsReturnedForAnAuthenticatedUser(string url)
+        {
+            // Arrange            
+            var client = _factory.WithWebHostBuilder(builder =>
+            {
+                builder.ConfigureTestServices(services =>
+                {
+                    services.AddAuthentication("Test")
+                        .AddScheme<AuthenticationSchemeOptions, TestAuthHandler>(
+                            "Test", options => { });
+                });
+            })
+                .CreateClient(new WebApplicationFactoryClientOptions
+                {
+                    AllowAutoRedirect = false,
+                });
+            
+            client.DefaultRequestHeaders.Authorization =
+                new AuthenticationHeaderValue("Test");
+
+            //Act
+            var response = await client.GetAsync(url);
+
+            // Assert
+            Assert.NotEqual(HttpStatusCode.Redirect, response.StatusCode);
+        }
+    }
+
+
+
+    public class TestAuthHandler : AuthenticationHandler<AuthenticationSchemeOptions>
+    {
+        public TestAuthHandler(IOptionsMonitor<AuthenticationSchemeOptions> options,
+            ILoggerFactory logger, UrlEncoder encoder, ISystemClock clock)
+            : base(options, logger, encoder, clock)
+        {
+        }
+
+        protected override Task<AuthenticateResult> HandleAuthenticateAsync()
+        {
+            var claims = new[] { new Claim(ClaimTypes.Name, "Test user") };
+            var identity = new ClaimsIdentity(claims, "Test");
+            var principal = new ClaimsPrincipal(identity);
+            var ticket = new AuthenticationTicket(principal, "Test");
+
+            var result = AuthenticateResult.Success(ticket);
+
+            return Task.FromResult(result);
         }
     }
 }
