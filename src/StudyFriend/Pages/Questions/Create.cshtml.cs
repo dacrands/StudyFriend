7c9e6f04a66c3aa90236568d3ec09c7d947b2de8
Rondinele Guimaraes <rondineleg@outlook.com>
2022-09-13 22:06:39 -0300

    Merge develop to main (#3)
    * Change .Net version to 6.0 (#1)
    
    Changing framewok version to latest
    
    * Feat/change structure and folders clean unused files (#2)
    
    * Removing files and unuseds references
    
    * Change structure folders

---
+++ src/StudyFriend/Pages/Questions/Create.cshtml.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StudyFriend.Data;
+using StudyFriend.Models;
+using System.Threading.Tasks;
+
+namespace StudyFriend.Pages.Questions
+{
+    public class CreateModel : TopicNamePageModel
+    {
+        private readonly StudyFriendContext _context;
+
+        public CreateModel(StudyFriendContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet(int? topicId)
+        {
+            if (topicId != null)
+            {
+                PopulateTopicsDropDownList(_context, topicId);
+                return Page();
+            }
+            PopulateTopicsDropDownList(_context);
+            return Page();
+        }
+
+        [BindProperty]
+        public Question Question { get; set; }
+
+        public async Task<IActionResult> OnPostAsync(int? topicId)
+        {
+            if (!ModelState.IsValid)
+            {
+                PopulateTopicsDropDownList(_context, topicId);
+                return Page();
+            }
+
+            var emptyQuestion = new Question();
+
+            if (await TryUpdateModelAsync(
+                emptyQuestion,
+                "question",
+                s => s.QuestionID, s => s.TopicID, s => s.Body, s => s.Topic))
+            {
+                _context.Question.Add(emptyQuestion);
+                await _context.SaveChangesAsync();
+                return RedirectToPage("../Questions/Details", new { id = emptyQuestion.QuestionID });
+            }
+
+            PopulateTopicsDropDownList(_context, emptyQuestion.QuestionID);
+            return Page();
+        }
+    }
+}
